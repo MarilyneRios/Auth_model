2baf91a30207f9ca965d8b32357f87d4a72b5a50
MarilyneRios <rios.marilyne@gmail.com>
2024-04-27 19:02:01 +0200

    retour code d'origine

--- api/index.js
+++ api/index.js
@@ -1,41 +1,31 @@
 //index.js
 import path from 'path';
+import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import express from 'express';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
-import cors from 'cors'; 
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
-const app = express(); 
+const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}
-)); 
-
-//gérer les données JSON et URL encodées dans les requêtes entrantes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-// Le terme “api”  est une convention lors de la création d’APIs Web.
 app.use('/api/users', userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    //indique au serveur où trouver et comment servir certains fichiers static
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-    // route générique qui correspond à toutes les requêtes GET
-    app.get('*', (req, res) =>
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
   );
 } else {
@@ -44,11 +34,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.get('/', (req, res) => res.send('Server is ready'));
-
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server Started on port ${port}`));
-
-export default app;
+app.listen(port, () => console.log(`Server started on port ${port}`));
--- api/utils/generateToken.js
+++ api/utils/generateToken.js
@@ -1,4 +1,3 @@
-//generateToken.js
 import jwt from 'jsonwebtoken';
 
 const generateToken = (res, userId) => {
@@ -8,13 +7,10 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development',
-    secure: process.env.NODE_ENV !== 'production',
-
-    sameSite: 'strict', 
-    maxAge: 30 * 24 * 60 * 60 * 1000, 
+    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+    sameSite: 'strict', // Prevent CSRF attacks
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
-  
 };
 
 export default generateToken;
