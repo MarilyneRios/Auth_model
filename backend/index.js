0130d2da96453e6df689c7d77710ad5731c94371
MarilyneRios <rios.marilyne@gmail.com>
2024-05-15 11:13:19 +0200

    modif header avec credentials true

--- backend/index.js
+++ backend/index.js
@@ -50,9 +50,9 @@ if (process.env.NODE_ENV === "production") {
 
 //--------------------middleware qui ajoute les en-têtes -----------------
 app.use((req, res, next) => {
-  //res.header('Access-Control-Allow-Origin', '*'); 
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true'); // credentials true comme dans le front
   
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
@@ -61,6 +61,8 @@ app.use((req, res, next) => {
   
   next();
 });
+
+//--------------------middleware qui ajoute les en-têtes -----------------
 
 
 app.use(notFound);
