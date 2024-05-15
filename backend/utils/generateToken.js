759d77c4e0a14f3fdf76aed78464adcc05b8d389
MarilyneRios <rios.marilyne@gmail.com>
2024-05-15 11:03:42 +0200

    preparattion render sameSite: 'none',dans genrate token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,8 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-    sameSite: 'strict', // Prevent CSRF attacks
+    //sameSite: 'strict', // Prevent CSRF attacks
+    sameSite: 'none', //si 2 domaine différent
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
