8f90c10c4b297f17564c442863690aab8c7d4920
MarilyneRios <rios.marilyne@gmail.com>
2024-05-15 10:57:29 +0200

    preparattion render ajout middleware pour les entête

--- backend/index.js
+++ backend/index.js
@@ -48,6 +48,21 @@ if (process.env.NODE_ENV === "production") {
 }
 // --------------------------deployment------------------------------
 
+//--------------------middleware qui ajoute les en-têtes -----------------
+app.use((req, res, next) => {
+  //res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  
+  next();
+});
+
+
 app.use(notFound);
 app.use(errorHandler);
 
