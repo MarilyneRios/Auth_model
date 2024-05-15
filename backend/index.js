89e106b2a41dcfbae2906da478e558273de0a5c5
Rios marilyne <rios.marilyne@gmail.com>
2024-05-15 17:56:42 +0200

    adresses localhos 2

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const adresses = ["http://localhost:3000", "*"];
 
 connectDB();
 
-const app = express
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
