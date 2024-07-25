dd013796b3afd6df8a88efd9b2b9d6546d1f88b3
Rios marilyne <rios.marilyne@gmail.com>
2024-07-25 10:23:38 +0200

    test build dans package

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,47 +1,9 @@
-// apiSlice.js
-/*
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-//configuration requête
-//const baseQuery = fetchBaseQuery({ baseUrl: '' });
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://auth-model.onrender.com/' });
+const baseQuery = fetchBaseQuery({ baseUrl: '' });
 
-
-//création d'API
 export const apiSlice = createApi({
   baseQuery,
-  tagTypes: ['User'], 
-  endpoints: (builder) => ({
-   
-  }),
-});
-*/
-
-//-------------------envoyer les cookies d'un domaine a.domain.ltd vers un domaine b.domain.ltd---------
-// apiSlice.js
-import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
-
-//configuration requête
-const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://auth-model.onrender.com/',//
-  prepareHeaders: (headers, { getState }) => {
-    // Use the `getState` function to get the current state
-    const { user } = getState();
-    console.log(user); 
-    // token
-    if (user && user.token) {
-      headers.set('authorization', `Bearer ${user.token}`);
-    }
-    return headers;
-  },
-  credentials: 'include', 
-});
-
-//création d'API
-export const apiSlice = createApi({
-  baseQuery,
-  tagTypes: ['User'], 
-  endpoints: (builder) => ({
-    // Vos endpoints vont ici
-  }),
+  tagTypes: ['User'],
+  endpoints: (builder) => ({}),
 });
